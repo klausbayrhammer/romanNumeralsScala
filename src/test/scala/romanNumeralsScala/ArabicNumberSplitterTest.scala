3ef6ed811e3373a98d70635446f34354a8d556e4
klausbayrhammer <extern.klaus.bayrhammer@porscheinformatik.com>
2014-02-20 23:06:39 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	README
    	pom.xml
    	src/main/scala/romanNumeralsScala/ArabicNumberSplitter.scala
    	src/main/scala/romanNumeralsScala/RomanNumerals.scala
    	src/test/scala/romanNumeralsScala/ArabicNumberSplitterTest.scala
    	src/test/scala/romanNumeralsScala/RomanNumeralsTest.scala

--- src/test/scala/romanNumeralsScala/ArabicNumberSplitterTest.scala
+++ src/test/scala/romanNumeralsScala/ArabicNumberSplitterTest.scala
@@ -1,6 +1,5 @@
 package romanNumeralsScala
 
-<<<<<<< HEAD
 import org.scalatest.{Matchers, FunSuite}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -24,27 +23,5 @@ class ArabicNumberSplitterTest extends FunSuite with Matchers {
   test("An arabic 123 should be a List(3, 20, 100)") {
     ArabicNumberSplitter(123).constituentParts should be (List(100, 20, 3))
   }
-=======
-import romanNumeralsScala.RomanNumerals
-
-@RunWith(classOf[JUnitRunner])
-class ArabicNumberSplitterTest extends FunSuite with ShouldMatchers {
-
-  test("An arabic 1 should be a roman I") {
-    RomanNumerals.toRomanNumber(1) should be ("I")
-  }
-
-  test("An arabic 5 should be a roman V") {
-    RomanNumerals.toRomanNumber(5) should be ("V")
-  }
-
-  test("An arabic 10 should be a roman X") {
-    RomanNumerals.toRomanNumber(10) should be ("X")
-  }
-
-//  test("An arabic 2 should be a roman II") {
-//    RomanNumerals.toRomanNumber(11) should be ("XI")
-//  }
->>>>>>> origin/master
 
 }
--- src/test/scala/romanNumeralsScala/RomanNumeralsTest.scala
+++ src/test/scala/romanNumeralsScala/RomanNumeralsTest.scala
@@ -1,6 +1,5 @@
 package romanNumeralsScala
 
-<<<<<<< HEAD
 import org.scalatest.{Matchers, FunSuite}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -31,31 +30,4 @@ class RomanNumeralsTest extends FunSuite with TableDrivenPropertyChecks with Mat
     (arabic: Int, roman: String) =>
     RomanNumerals.toRomanNumber(arabic) should be(roman)
   }
-=======
-import org.scalatest.FunSuite
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers
-import romanNumeralsScala.RomanNumerals
-
-@RunWith(classOf[JUnitRunner])
-class RomanNumeralsTest extends FunSuite with ShouldMatchers {
-
-  test("An arabic 1 should be a roman I") {
-    RomanNumerals.toRomanNumber(1) should be ("I")
-  }
-
-  test("An arabic 5 should be a roman V") {
-    RomanNumerals.toRomanNumber(5) should be ("V")
-  }
-
-  test("An arabic 10 should be a roman X") {
-    RomanNumerals.toRomanNumber(10) should be ("X")
-  }
-
-  test("An arabic 2 should be a roman II") {
-    RomanNumerals.toRomanNumber(11) should be ("XI")
-  }
-
->>>>>>> origin/master
 }
